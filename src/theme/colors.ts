7e624490723ae75e90ad71065037fabf4a315858
operaswap <admin@operaswap.finance>
2021-06-10 23:40:33 +0800

    update colours

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,10 +2,10 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#aa8929",
-  primaryBright: "#aa8929",
-  primaryDark: "#aa8929",
-  secondary: "#7645D9",
+  primary: "#000000",
+  primaryBright: "#000000",
+  primaryDark: "#000000",
+  secondary: "#323232",
   success: "#31D0AA",
   warning: "#FFB237",
 };
@@ -23,9 +23,9 @@ export const lightColors: Colors = {
   invertedContrast: "#FFFFFF",
   input: "#eeeaf4",
   tertiary: "#EFF4F5",
-  text: "#452A7A",
-  textDisabled: "#BDC2C4",
-  textSubtle: "#8f80ba",
+  text: "#000000",
+  textDisabled: "#9d9d9d",
+  textSubtle: "#2b2b2b",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
@@ -36,7 +36,8 @@ export const lightColors: Colors = {
 export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
-  secondary: "#9A6AFF",
+  primary: "#727272",
+  secondary: "#626262",
   background: "#343135",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
